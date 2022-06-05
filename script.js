ce69034875006fabe614a7f21163a75c85a43a2f
laterNaT <105680553+laterNaT@users.noreply.github.com>
2022-06-05 18:21:17 +0200

    Add operators

---
+++ script.js
@@ -0,0 +1,35 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, num1, num2) {
+  if (operator === '+') {
+    return add(num1, num2);
+  }
+
+  if (operator === '-') {
+    return subtract(num1, num2);
+  }
+
+  if (operator === '*') {
+    return multiply(num1, num2);
+  }
+
+  if (operator === '/') {
+    return divide(num1, num2);
+  }
+
+  return 'Error';
+}
