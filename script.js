e0d10782f8829fac7e1f3626c2a35a1cfc246637
laterNaT <105680553+laterNaT@users.noreply.github.com>
2022-06-06 17:40:44 +0200

    Implement operations with two user inputs

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let userInput = [];
+
 function add(a, b) {
   return a + b;
 }
@@ -23,7 +25,7 @@ function operate(operator, num1, num2) {
     return subtract(num1, num2);
   }
 
-  if (operator === '*') {
+  if (operator === 'X') {
     return multiply(num1, num2);
   }
 
@@ -33,3 +35,41 @@ function operate(operator, num1, num2) {
 
   return 'Error';
 }
+
+function isValidOperator(operator) {
+  if (typeof operator === 'string') {
+    return operator.match(/[/X+-]/) != null;
+  }
+  return false;
+}
+
+function isValidNumber(str) {
+  return str.match(/\d/) != null;
+}
+
+function isValidOperation() {
+  return userInput.length === 3
+    && typeof userInput[0] === 'number'
+    && isValidOperator(userInput[1])
+    && typeof userInput[2] === 'number';
+}
+
+function btnClickHandler(e) {
+  const value = e.target.textContent;
+  if (isValidNumber(value)) {
+    userInput.push(Number(value));
+  } else if (isValidOperator(value)) {
+    userInput.push(value);
+  }
+
+  if (isValidOperation()) {
+    const result = operate(userInput[1], userInput[0], userInput[2]);
+    document.getElementById('screen-text').innerText = result;
+    userInput = [];
+  }
+}
+
+const btns = document.querySelectorAll('.buttons button');
+btns.forEach((btn) => {
+  btn.addEventListener('click', btnClickHandler);
+});
